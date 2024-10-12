b3508cc3e555b8f3e3f94f2a3b373d61f9fdaf17
MikeWilly5125 <mike.inspeferreira@gmail.com>
2024-10-12 16:40:18 -0300

    teste1

--- cypress/e2e/testeIntegration.cy.js
+++ cypress/e2e/testeIntegration.cy.js
@@ -2,6 +2,12 @@ describe('testeIntegration', () => {
     it('teste1', () => {
 
         cy.visit('www.google.com.br')
+        
+    });
+
+    it('teste2', () => {
+
+        cy.visit('www.google.com.br')
         cy.log('error'),
         cy.get('error')
     });
